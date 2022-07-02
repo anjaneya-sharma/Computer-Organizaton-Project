19e4fc2fcafcbb2efeee3a7fa57fb2f135fff688
anjaneya-sharma <anjaneya21449@iiitd.ac.in>
2022-07-02 23:05:27 +0530

    Create simple-assembler.py

---
+++ simple-assembler.py
@@ -0,0 +1,24 @@
+import sys
+from sys import stdin
+
+opcodes={"add":"10000","sub":"10001","mov":"10010","ld":"10100","st":"10101","mul":"10110","div":"10111","rs":"11000","ls":"110001","xor":"11010","or":"11011","and":"11100","not":"11101","cmp":"111110","jmp":"11111","jlt":"01100","jgt":"01101","je":"01111","hlt":"01010"}
+
+Reg_dict={"r0":"000","r1":"001","r2":"010","r3":"011","r4":"100","r5":"101","r6":"110","flags":"111"}
+
+Reg_typ_A=["add","sub","mul","xor","or","and"]
+Reg_typ_B=["mov","rs","ls"]
+Reg_typ_C=["mov","div","not","cmp"]
+Reg_typ_D=["ld","st"]
+Reg_typ_E=["jmp","jlt","jgt","je"]
+
+Flags=["0","0","0","0"] #(V,L,G,E)
+
+Reg_val=["0","0","0","0","0","0","0","0"]
+
+def flag_setter(v=0,l=0,g=0,e=0):
+    Flags[0]=str(v)
+    Flags[1]=str(l)
+    Flags[2]=str(g)
+    Flags[3]=str(e)
+    #setting value of flag register
+    Reg_val[7]="000000000000"+Flags[0]+Flags[1]+Flags[2]+Flags[3]
